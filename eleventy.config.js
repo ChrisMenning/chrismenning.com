41bba95d45e4d440b067c43f4a88309eccd0f672
ChrisMenning <chrismenning@gmail.com>
2026-03-14 17:20:07 -0500

    Add NWTC Metaversity, self-hosting projects; tag browse pages; NWTC year fix

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,17 @@ module.exports = function (eleventyConfig) {
     tag.split("-").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
   );
 
+  // Collect all unique projectTags values across all project files
+  eleventyConfig.addCollection("projectTagList", function (collectionApi) {
+    const tags = new Set();
+    collectionApi.getAll().forEach(item => {
+      if (item.data.projectTags) {
+        item.data.projectTags.forEach(tag => tags.add(tag));
+      }
+    });
+    return [...tags].sort();
+  });
+
   // Pass static assets through unchanged
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
